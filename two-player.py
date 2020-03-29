2e44511af8bc5869334871b0c4c4bce9f5e182dd
amonti <amonti@localhost.localdomain>
2020-03-29 16:36:31 +0430

    os library added to clean screen

--- two-player.py
+++ two-player.py
@@ -6,6 +6,7 @@ Updated: 2020
 Author: Arian Montazeri
 """
 
+import os
 import functions as func
 table, a, counter = [], [], 0
 win = ((0, 1, 2), (0, 3, 6), (2, 4, 6),
@@ -21,6 +22,7 @@ func.Print_logo()
 def main(counter):
     try:
         while True:
+            os.system('clear')
             func.Print_table(table)
             player_X = (int(input("Player X : ")) - 1)
             table[player_X] = "X"
@@ -32,6 +34,7 @@ def main(counter):
                 break
             if func.check_tie(counter, table, main, a):
                 break
+            os.system('clear')
             func.Print_table(table)
             player_O = (int(input("Player O : ")) - 1)
             table[player_O] = "O"
